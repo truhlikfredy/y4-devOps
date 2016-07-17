e0a4af0ac79136e969f044dcfdd9c0b0a4ac5f40
Anton Krug <antonkrug@gmail.com>
2016-07-17 16:26:37 +0100

    Adding comments how to tweak the runtime

--- rundeck-run.sh
+++ rundeck-run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#http://rundeck.org/docs/administration/configuring-ssl.html
+
 cd /optfp/ops/rundeck
+
+#java -cp server/lib/jetty-all-7.6.0.v20120127.jar org.eclipse.jetty.util.security.Password [username] [password]
 
 java -jar rundeck-launcher-2.6.8.jar
