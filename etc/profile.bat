92830d0e8182bbee9e8ac3cdc1d16341723436e0
Anton Krug <antonkrug@gmail.com>
2016-07-17 13:50:30 +0100

    First commit

---
+++ etc/profile.bat
@@ -0,0 +1,13 @@
+
+set RDECK_BASE=/optfp/ops/rundeck
+
+set JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre
+
+:: Unsetting JRE_HOME to ensure there is no conflict with JAVA_HOME
+(set JRE_HOME=)
+
+set Path=%JAVA_HOME%\bin;%RDECK_BASE%\tools\bin;%Path%
+
+set RDECK_SSL_OPTS="-Djavax.net.ssl.trustStore=%RDECK_BASE%\etc\truststore -Djavax.net.ssl.trustStoreType=jks -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol"
+set RDECK_CLI_OPTS=-Xms64m -Xmx128m
+set RD_LIBDIR=%RDECK_BASE%\tools\lib
---
+++ rundeck-run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /optfp/ops/rundeck
+
+java -jar rundeck-launcher-2.6.8.jar
